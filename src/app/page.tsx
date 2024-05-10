315789ace1acfe82839e6870e8cbbca8862fc43a
Théo Gândara <theogandara@gmail.com>
2024-05-10 23:54:18 +0100

    Feat:improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-import Header from "@/components/Header";
-import Nav from "@/components/Nav";
-
-const items = [
-  { name: "home", url: "/" },
-  { name: "projetos", url: "/projects" },
-  { name: "sobre", url: "/about" },
-  { name: "objetivos", url: "/objectives" },
-  { name: "planner", url: "/planner" },
-  { name: "blog", url: "blog" },
-  { name: "weeklyCheck", url: "/weeklyCheck" },
-  { name: "discord", url: "/discord" },
-];
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-12 p-24 pt-0">
-      <Header />
-      <Nav items={items} />
-    </main>
+    <main className="flex min-h-screen flex-col gap-12 p-24 pt-0">home</main>
   );
 }
--- src/pages/about/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function About() {
-  return (
-    <main className="flex min-h-screen flex-col gap-12 p-24 pt-0">about</main>
-  );
-}
