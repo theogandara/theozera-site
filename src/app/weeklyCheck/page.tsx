c83dd1c0be542ee4177640476e4b2d6957fc9a6a
Théo Gândara <theogandara@gmail.com>
2024-05-11 01:03:41 +0100

    Feat: added weekly check

--- src/app/weeklyCheck/page.tsx
+++ src/app/weeklyCheck/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+
 export default function WeeklyCheck() {
   return (
-    <main className="flex min-h-screen flex-col gap-12 p-24 pt-0">
-      WeeklyCheck
+    <main className="flex min-h-screen flex-col gap-12 md:p-24 pt-0">
+      {/* <h1 className="text-4xl font-jet text-center">Weekly Check</h1> */}
+
+      <h1 className="text-4xl font-jet text-center text-blue-500">
+        <a
+          className="text-blue-500 hover:underline"
+          href="/theozera-weekly-check.pdf"
+        >
+          Clique para baixar
+        </a>
+      </h1>
+
+      <div className="flex ">
+        <Image
+          width={1400}
+          quality={100}
+          height={100}
+          src="/weekly-ad.png"
+          alt="test"
+        />
+      </div>
     </main>
   );
 }
