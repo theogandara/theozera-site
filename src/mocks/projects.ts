e104bead0b1e28620dce53c5b5b6cf743d5e01c2
Théo Gândara <theogandara@gmail.com>
2024-06-02 00:02:44 +0100

    Feat: change projects order

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,8 +1,8 @@
 export const projects = [
   {
-    name: "Ivo Vaz Imobiliária",
-    image: "/ivo-vaz.png",
-    link: "https://imobiliaria-white-label.vercel.app/",
+    name: "Site da Nuxt IT",
+    image: "/nuxt.png",
+    link: "https://nuxt-lp.vercel.app/",
   },
   {
     name: "App Ifuture",
@@ -10,18 +10,18 @@ export const projects = [
     link: "https://ifuture-wine.vercel.app/",
   },
   {
-    name: "Site da Nuxt IT",
-    image: "/nuxt.png",
-    link: "https://nuxt-lp.vercel.app/",
+    name: "HouzFy",
+    image: "/houzfy.png",
+    link: "https://houzfy-lp.vercel.app/",
+  },
+  {
+    name: "Ivo Vaz Imobiliária",
+    image: "/ivo-vaz.png",
+    link: "https://imobiliaria-white-label.vercel.app/",
   },
   {
     name: "Solução FoodFy",
     image: "/foodfy.png",
     link: "",
-  },
-  {
-    name: "HouzFy",
-    image: "/houzfy.png",
-    link: "https://houzfy-lp.vercel.app/",
   },
 ];
