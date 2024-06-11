cce253a955693784c70736ce6c3ecdc31f5a1c8f
Théo Gândara <theogandara@gmail.com>
2024-06-11 23:56:35 +0100

    Feat: added icons

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { GithubLogo } from "@phosphor-icons/react";
+import {
+  GithubLogo,
+  GitlabLogo,
+  LinkedinLogo,
+  InstagramLogo,
+  YoutubeLogo,
+} from "@phosphor-icons/react";
 import Link from "next/link";
 
 export default function Links() {
@@ -10,14 +16,34 @@ export default function Links() {
       url: "https://www.github.com/theogandara",
       icon: <GithubLogo size={24} />,
     },
+    {
+      name: "Gitlab",
+      url: "https://www.github.com/theogandara",
+      icon: <GitlabLogo size={24} />,
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.github.com/theogandara",
+      icon: <LinkedinLogo size={24} />,
+    },
+    {
+      name: "Instagram",
+      url: "https://www.github.com/theogandara",
+      icon: <InstagramLogo size={24} />,
+    },
+    {
+      name: "Youtube",
+      url: "https://www.github.com/theogandara",
+      icon: <YoutubeLogo size={24} />,
+    },
   ];
 
   return (
     <main className="flex flex-col gap-12 pt-0">
       <div className="w-[260px] sm:w-[520px] lg:w-[1040px] flex flex-wrap mx-auto">
-        <ul>
+        <ul className="flex flex-col lg:flex-row gap-5 lg:justify-between lg:w-full">
           {links.map((link) => (
-            <li key={link.url}>
+            <li className="flex" key={link.url}>
               <Link target="_blank" href={link.url} className="flex gap-4">
                 <span>{link.icon}</span>
                 <span>{link.name}</span>
